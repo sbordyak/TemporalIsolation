23bd736f6d9e516a81dd2651cfbe2b579bd8c690
imcnulty837 <xandermcnulty@gmail.com>
2022-10-06 16:32:26 -0400

    added files

---
+++ Version1.0/Scheduler.m
@@ -0,0 +1,10 @@
+classdef Scheduler
+    properties
+        
+    end
+    methods
+        function self = Scheduler()
+
+        end
+    end
+end
---
+++ Version1.0/septemberPlot.m
@@ -0,0 +1,3 @@
+function [] = septemberPlot(model)
+    
+end
