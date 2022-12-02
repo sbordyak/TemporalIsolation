77569db59442855de9bcc5844e28555796535dc8
imcnulty837 <xandermcnulty@gmail.com>
2022-12-02 12:36:55 -0500

    Added comments

--- Version1.0/testingScript.m
+++ Version1.0/testingScript.m
@@ -1,7 +1,5 @@
-%% TODO:
-%   Create stable build with every variable staying the same except
-%   changing variable.
 
+## Change these to change the variables
 emergence_mean_mean_A = 70;
 emergence_mean_std_A = 5;
 emergence_std_mean_A = 5;
@@ -26,7 +24,35 @@ plong_mean_mean = 14;
 plong_mean_std = 2;
 plong_std_mean = 2;
 plong_std_std = 0.5;
+            
+## Change these to change the settings of the model calculation
 
+# options = {type of egg ratio calculation, type of distribution used for PDF, type of distribution used for CDF, egg site number, egg layer number, include plant variables(1) or dont(0)}
+options = {"ratio1", 'normal', 'normal', 500, 219, 1};
+
+# number of tests to run for randomized testing
+tests = 10;
+
+# range of test values to use
+test_range = -19:20;
+
+# array of changing variables (1) and stationary variables (0) in order of: 
+#   [emergence_mean_A, emergence_STD_A, leaf_flush_mean_A, leaf_flush_STD_A, lifespan_mean_A, lifespan_STD_A, plong_mean_A, plong_STD_A,
+#    emergence_mean_B, emergence_STD_B, leaf_flush_mean_B, leaf_flush_STD_B, lifespan_mean_B, lifespan_STD_B, plong_mean_B, plong_STD_B]
+boolean = [1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+
+# which of the 1 values in the boolean array is changing, 1st(1), 2nd(2), etc
+changing_variable = 1;
+
+# base values for each variable in the order of:
+#   [emergence_mean_A, emergence_STD_A, leaf_flush_mean_A, leaf_flush_STD_A, lifespan_mean_A, lifespan_STD_A, plong_mean_A, plong_STD_A,
+#    emergence_mean_B, emergence_STD_B, leaf_flush_mean_B, leaf_flush_STD_B, lifespan_mean_B, lifespan_STD_B, plong_mean_B, plong_STD_B]
+seed = [70 5 10 5 80 5 50 5 70 5 7 5 50 5 20 5];
+
+# switch for incremental(1) or RNG(0) test
+incremental_or_RNG = 1;
+
+## Don't change these, these are for variable loading
 values_mat = {};
 values_mat{end+1} = [emergence_mean_mean_A, emergence_mean_std_A; emergence_std_mean_A, emergence_std_std_A];
 values_mat{end+1} = [lifespan_mean_mean, lifespan_mean_std; lifespan_std_mean, lifespan_std_std];
@@ -36,21 +62,8 @@ values_mat{end+1} = [emergence_mean_mean_B, emergence_mean_std_B; emergence_std_
 values_mat{end+1} = [lifespan_mean_mean, lifespan_mean_std; lifespan_std_mean, lifespan_std_std];
 values_mat{end+1} = [leaf_flush_mean_mean_B, leaf_flush_mean_std_B; leaf_flush_std_mean_B, leaf_flush_std_std_B];
 values_mat{end+1} = [plong_mean_mean, plong_mean_std; plong_std_mean, plong_std_std];
-                
-options = {"ratio1", 'normal', 'normal', 500, 219, 1};
-tests = 10;
-test_range = -19:20;
-boolean = [1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
-changing_variable = 1;
-seed = [70 5 10 5 80 5 50 5 70 5 7 5 50 5 20 5];
-incremental_or_RNG = 1;
 
 model = TemporalIsolationModel(sum(boolean(:)==1), options, seed, boolean, values_mat, test_range, tests, changing_variable, incremental_or_RNG);
 
 septemberPlot(model, 0, incremental_or_RNG);
-
-
-
-
-
 
